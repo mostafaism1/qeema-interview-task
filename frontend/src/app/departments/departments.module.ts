52fb8e78c1e3329b113352ee44e69791f50beea6
mostafa <me.cssign@gmail.com>
2021-06-27 01:40:39 +0200

    Import FlexLayoutModule in DepartmentModule

--- frontend/src/app/departments/departments.module.ts
+++ frontend/src/app/departments/departments.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
-
+import { DepartmentGridComponent } from './department-grid/department-grid.component';
 import { DepartmentsRoutingModule } from './departments-routing.module';
 import { DepartmentsComponent } from './departments.component';
-import { DepartmentGridComponent } from './department-grid/department-grid.component';
-
-
 
 @NgModule({
-  declarations: [
-    DepartmentsComponent,
-    DepartmentGridComponent
-  ],
+  declarations: [DepartmentsComponent, DepartmentGridComponent],
   imports: [
     CommonModule,
     DepartmentsRoutingModule,
-    MatGridListModule
-  ]
+    MatGridListModule,
+    FlexLayoutModule,
+  ],
 })
-export class DepartmentsModule { }
+export class DepartmentsModule {}
