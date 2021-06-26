6629ab14c68a926523e1ad44798765d7ee24a136
mostafa <me.cssign@gmail.com>
2021-06-26 22:23:53 +0200

    Implement percentage-donut component

--- frontend/src/app/charts/percentage-donut/percentage-donut.component.ts
+++ frontend/src/app/charts/percentage-donut/percentage-donut.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AnimationModel } from '@syncfusion/ej2-angular-progressbar';
 
 @Component({
   selector: 'pm-percentage-donut',
   templateUrl: './percentage-donut.component.html',
-  styleUrls: ['./percentage-donut.component.scss']
+  styleUrls: ['./percentage-donut.component.scss'],
 })
 export class PercentageDonutComponent implements OnInit {
+  @Input() percent: number;
+  readonly animation: AnimationModel = {
+    enable: true,
+    duration: 2000,
+    delay: 0,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
