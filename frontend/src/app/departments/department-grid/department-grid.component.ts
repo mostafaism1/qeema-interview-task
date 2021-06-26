9d9248986d615be8a0b4238d97d752dc1a8c6e17
mostafa <me.cssign@gmail.com>
2021-06-27 00:08:58 +0200

    Generate a DepartmentGrid component

---
+++ frontend/src/app/departments/department-grid/department-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-department-grid',
+  templateUrl: './department-grid.component.html',
+  styleUrls: ['./department-grid.component.scss']
+})
+export class DepartmentGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/departments/departments.module.ts
+++ frontend/src/app/departments/departments.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DepartmentsRoutingModule } from './departments-routing.module';
 import { DepartmentsComponent } from './departments.component';
+import { DepartmentGridComponent } from './department-grid/department-grid.component';
 
 
 @NgModule({
   declarations: [
-    DepartmentsComponent
+    DepartmentsComponent,
+    DepartmentGridComponent
   ],
   imports: [
     CommonModule,
