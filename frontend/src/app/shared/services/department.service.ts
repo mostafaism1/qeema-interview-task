7f6cb0c548268ab2ef7659fc8222fba98fedbe1c
mostafa <me.cssign@gmail.com>
2021-06-26 19:59:27 +0200

    Define an abstract DepartmentService

---
+++ frontend/src/app/shared/services/department.service.ts
@@ -0,0 +1,9 @@
+import { Observable } from 'rxjs';
+import { DepartmentMilestones } from '../models/department-milestones.model';
+import { Department } from '../models/department.model';
+
+
+export abstract class DepartmentService {
+  abstract getAllDepartments(): Observable<Department[]>;
+  abstract getDepartmentMilestones(departmentId: number): Observable<DepartmentMilestones | undefined>;
+}
