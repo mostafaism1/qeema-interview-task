109d088d227f742aa4c08a051e9a4c5d89555bf7
mostafa <me.cssign@gmail.com>
2021-06-27 00:03:58 +0200

    Generate a Departments lazy-loaded feature module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'departments', loadChildren: () => import('./departments/departments.module').then(m => m.DepartmentsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
