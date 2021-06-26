c525dcebde1ad6ba0ca22ac478fd28c4451838bf
mostafa <me.cssign@gmail.com>
2021-06-26 21:14:37 +0200

    Import ProgressBarModule

--- frontend/src/app/charts/charts.module.ts
+++ frontend/src/app/charts/charts.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProgressBarModule } from '@syncfusion/ej2-angular-progressbar';
 import { PerformanceHorizontalBarComponent } from './performance-horizontal-bar/performance-horizontal-bar.component';
 
-
-
 @NgModule({
-  declarations: [
-    PerformanceHorizontalBarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PerformanceHorizontalBarComponent],
+  imports: [CommonModule, ProgressBarModule],
 })
-export class ChartsModule { }
+export class ChartsModule {}
