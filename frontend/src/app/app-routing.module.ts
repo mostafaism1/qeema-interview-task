a57b586067e01dc4f28bc401e33701431ec7da62
mostafa <me.cssign@gmail.com>
2021-06-27 00:06:51 +0200

    Redirect home page to departments

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'departments', loadChildren: () => import('./departments/departments.module').then(m => m.DepartmentsModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'departments', pathMatch: 'full' },
+  {
+    path: 'departments',
+    loadChildren: () =>
+      import('./departments/departments.module').then(
+        (m) => m.DepartmentsModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
