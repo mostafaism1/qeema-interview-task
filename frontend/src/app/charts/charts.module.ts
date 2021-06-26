f0b91a60c3aa1c1556cd0dfc678e9ee865a6b341
mostafa <me.cssign@gmail.com>
2021-06-26 21:33:47 +0200

    Import FlexLayoutModule in ChartsModule

--- frontend/src/app/charts/charts.module.ts
+++ frontend/src/app/charts/charts.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProgressBarModule } from '@syncfusion/ej2-angular-progressbar';
 import { PerformanceHorizontalBarComponent } from './performance-horizontal-bar/performance-horizontal-bar.component';
 
 @NgModule({
   declarations: [PerformanceHorizontalBarComponent],
-  imports: [CommonModule, ProgressBarModule],
+  imports: [CommonModule, ProgressBarModule, FlexLayoutModule],
 })
 export class ChartsModule {}
