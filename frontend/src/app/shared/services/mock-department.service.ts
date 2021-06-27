fb94224dbdc274d76c2fcd82a741ab035571a941
mostafa <me.cssign@gmail.com>
2021-06-27 02:41:37 +0200

    Add @Injectable decoration to mock services

--- frontend/src/app/shared/services/mock-department.service.ts
+++ frontend/src/app/shared/services/mock-department.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DepartmentMilestones } from '../models/department-milestones.model';
 import { Department } from '../models/department.model';
 import { DepartmentService } from './department.service';
 
+@Injectable({
+  providedIn: null,
+})
 export class MockDepartmentService implements DepartmentService {
   constructor(private http: HttpClient) {}
 
--- frontend/src/app/shared/services/mock-section.service.ts
+++ frontend/src/app/shared/services/mock-section.service.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SectionPerformance } from '../models/section-performance.model';
 import { Section } from '../models/section.model';
 import { SectionService } from './section.service';
 
+@Injectable({
+  providedIn: null,
+})
 export class MockSectionService implements SectionService {
   constructor(private http: HttpClient) {}
 
