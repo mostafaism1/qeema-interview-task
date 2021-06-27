189b6fb636872cf0932204184633a3d7be341375
mostafa <me.cssign@gmail.com>
2021-06-27 02:26:49 +0200

    Import MatCardModule in ChartModule

--- frontend/src/app/charts/charts.module.ts
+++ frontend/src/app/charts/charts.module.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 import { ProgressBarModule } from '@syncfusion/ej2-angular-progressbar';
-import { PerformanceHorizontalBarComponent } from './performance-horizontal-bar/performance-horizontal-bar.component';
 import { PercentageDonutComponent } from './percentage-donut/percentage-donut.component';
 import { PerformanceHorizontalBarArrayComponent } from './performance-horizontal-bar-array/performance-horizontal-bar-array.component';
+import { PerformanceHorizontalBarComponent } from './performance-horizontal-bar/performance-horizontal-bar.component';
 import { StrategicThemesProgressComponent } from './strategic-themes-progress/strategic-themes-progress.component';
 
 @NgModule({
-  declarations: [PerformanceHorizontalBarComponent, PercentageDonutComponent, PerformanceHorizontalBarArrayComponent, StrategicThemesProgressComponent],
-  imports: [CommonModule, ProgressBarModule, FlexLayoutModule],
+  declarations: [
+    PerformanceHorizontalBarComponent,
+    PercentageDonutComponent,
+    PerformanceHorizontalBarArrayComponent,
+    StrategicThemesProgressComponent,
+  ],
+  imports: [CommonModule, ProgressBarModule, FlexLayoutModule, MatCardModule],
 })
 export class ChartsModule {}
