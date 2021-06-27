30b84fd6d111bc783f888b2ce34ed81a9e535f2a
mostafa <me.cssign@gmail.com>
2021-06-27 02:27:54 +0200

    Implement StrategicThemesProgress component

--- frontend/src/app/charts/strategic-themes-progress/strategic-themes-progress.component.ts
+++ frontend/src/app/charts/strategic-themes-progress/strategic-themes-progress.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Performance } from '../../shared/models/performance.model';
 
 @Component({
   selector: 'pm-strategic-themes-progress',
   templateUrl: './strategic-themes-progress.component.html',
-  styleUrls: ['./strategic-themes-progress.component.scss']
+  styleUrls: ['./strategic-themes-progress.component.scss'],
 })
 export class StrategicThemesProgressComponent implements OnInit {
+  @Input() performances: Performance[];
+  date: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  computeAveragePercent(performances: Performance[]): number {
+    return (
+      performances.map((p) => p.percent).reduce((a, b) => a + b) /
+      performances.length
+    );
   }
-
 }
