4ac6c373d42c3ced445696ff410674c211ecb11d
mostafa <me.cssign@gmail.com>
2021-06-27 02:29:04 +0200

    Import ChartsModule in DepartmentModule

--- frontend/src/app/departments/departments.module.ts
+++ frontend/src/app/departments/departments.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { ChartsModule } from '../charts/charts.module';
 import { DepartmentGridComponent } from './department-grid/department-grid.component';
 import { DepartmentsRoutingModule } from './departments-routing.module';
 import { DepartmentsComponent } from './departments.component';
@@ -13,6 +14,7 @@ import { DepartmentsComponent } from './departments.component';
     DepartmentsRoutingModule,
     MatGridListModule,
     FlexLayoutModule,
+    ChartsModule
   ],
 })
 export class DepartmentsModule {}
