dad346e9341b98dec60d686acd3666060bde51d2
mostafa <me.cssign@gmail.com>
2021-06-28 22:54:18 +0200

    Remove undefined type from DepartmentService.getDepartmentMilestones() algebraic return type

--- frontend/src/app/shared/services/department.service.ts
+++ frontend/src/app/shared/services/department.service.ts
@@ -2,8 +2,9 @@ import { Observable } from 'rxjs';
 import { DepartmentMilestones } from '../models/department-milestones.model';
 import { Department } from '../models/department.model';
 
-
 export abstract class DepartmentService {
   abstract getAllDepartments(): Observable<Department[]>;
-  abstract getDepartmentMilestones(departmentId: number): Observable<DepartmentMilestones | undefined>;
+  abstract getDepartmentMilestones(
+    departmentId: number
+  ): Observable<DepartmentMilestones>;
 }
