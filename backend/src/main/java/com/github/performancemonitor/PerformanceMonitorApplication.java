d05c1e949db6f120ed39b9d91d2377222c0b4a5a
mostafa <me.cssign@gmail.com>
2021-06-29 16:44:41 +0200

    Generate a Spring Boot backend application

---
+++ backend/src/main/java/com/github/performancemonitor/PerformanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.performancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceMonitorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/performancemonitor/PerformanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.performancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
