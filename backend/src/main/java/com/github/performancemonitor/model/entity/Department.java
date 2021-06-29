3ab32ee3a60d369b68aeb4fc919abcc681bba6b2
mostafa <me.cssign@gmail.com>
2021-06-30 00:00:02 +0200

    Define a Department entity

---
+++ backend/src/main/java/com/github/performancemonitor/model/entity/Department.java
@@ -0,0 +1,45 @@
+package com.github.performancemonitor.model.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "department")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @NotBlank
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Nullable
+    @Column(name = "logo_url")
+    private String logoUrl;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "department")
+    private List<Section> sections;
+}
