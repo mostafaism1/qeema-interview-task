ab6e65166ed66d60fead04d9f65b8e9cf696d89a
mostafa <me.cssign@gmail.com>
2021-06-30 04:14:16 +0200

    Add an endpoint to Department controller to get a department's sections

--- backend/src/main/java/com/github/performancemonitor/controller/DepartmentController.java
+++ backend/src/main/java/com/github/performancemonitor/controller/DepartmentController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.github.performancemonitor.model.dto.DepartmentDto;
+import com.github.performancemonitor.model.dto.SectionDto;
 import com.github.performancemonitor.model.mapper.DepartmentToDepartmentDtoMapper;
+import com.github.performancemonitor.model.mapper.SectionToSectionDtoMapper;
 import com.github.performancemonitor.service.DepartmentService;
+import com.github.performancemonitor.service.SectionService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,18 @@ import lombok.AllArgsConstructor;
 public class DepartmentController {
 
     private final DepartmentService departmentService;
-    private final DepartmentToDepartmentDtoMapper mapper;
+    private final SectionService sectionService;
+    private final DepartmentToDepartmentDtoMapper departmentMapper;
+    private final SectionToSectionDtoMapper sectionMapper;
 
     @GetMapping
     public List<DepartmentDto> getAllDepartments() {
-        return departmentService.getAllDepartments().stream().map(mapper).collect(Collectors.toList());
+        return departmentService.getAllDepartments().stream().map(departmentMapper).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}/sections")
+    public List<SectionDto> getDepartmentSections(@PathVariable(name = "id") long id) {
+        return sectionService.getDepartmentSections(id).stream().map(sectionMapper).collect(Collectors.toList());
     }
 
 }
