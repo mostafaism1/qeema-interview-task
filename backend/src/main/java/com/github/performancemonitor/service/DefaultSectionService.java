4215492671a98909449ac669eb0bf87b34532875
mostafa <me.cssign@gmail.com>
2021-06-30 03:52:41 +0200

    Implement a Section service

---
+++ backend/src/main/java/com/github/performancemonitor/service/DefaultSectionService.java
@@ -0,0 +1,25 @@
+package com.github.performancemonitor.service;
+
+import java.util.List;
+
+import com.github.performancemonitor.model.entity.Department;
+import com.github.performancemonitor.model.entity.Section;
+import com.github.performancemonitor.repository.DepartmentRepository;
+import com.github.performancemonitor.repository.SectionRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DefaultSectionService implements SectionService {
+
+    private final DepartmentRepository departmentRepository;
+    private final SectionRepository sectionRepository;
+
+    @Override
+    public List<Section> getDepartmentSections(long departmentId) {
+        Department department = departmentRepository.findById(departmentId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid department id"));
+        return sectionRepository.getByDepartmentOrderById(department);
+    }
+
+}
