59598a6c4f826de9de3ac599b7458c649e44b9ef
mostafa <me.cssign@gmail.com>
2021-06-30 08:48:14 +0200

    Add service DepartmentService.getDepartmentSections()

--- backend/src/main/java/com/github/performancemonitor/service/DepartmentService.java
+++ backend/src/main/java/com/github/performancemonitor/service/DepartmentService.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import com.github.performancemonitor.model.entity.Department;
 import com.github.performancemonitor.model.entity.DepartmentMilestones;
+import com.github.performancemonitor.model.entity.Section;
 
 public interface DepartmentService {
 
     List<Department> getAllDepartments();
 
     DepartmentMilestones getDepartmentMilestones(long departmentId);
+
+    List<Section> getDepartmentSections(long departmentId);
 
 }
