bc387ce57ed8164e341899b22329100ce20b9e99
mostafa <me.cssign@gmail.com>
2021-06-30 12:01:40 +0200

    Implement DefaultDepartmentService

--- frontend/src/app/shared/services/default-department.service.ts
+++ frontend/src/app/shared/services/default-department.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DepartmentMilestones } from '../models/department-milestones.model';
+import { Department } from '../models/department.model';
+import { DepartmentService } from './department.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DefaultDepartmentService {
+export class DefaultDepartmentService implements DepartmentService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllDepartments(): Observable<Department[]> {
+    return this.http.get<Department[]>('/api/departments');
+  }
+
+  getDepartmentMilestones(
+    departmentId: number
+  ): Observable<DepartmentMilestones> {
+    return this.http.get<DepartmentMilestones>(
+      '/api/departments/' + departmentId + '/milestones'
+    );
+  }
 }
