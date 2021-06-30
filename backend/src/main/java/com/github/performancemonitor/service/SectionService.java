a5dfc72686d93b4cc5884daa4b57fd18b1247376
mostafa <me.cssign@gmail.com>
2021-06-30 08:53:44 +0200

    Remove service SectionService.getDepartmentSections()

--- backend/src/main/java/com/github/performancemonitor/service/SectionService.java
+++ backend/src/main/java/com/github/performancemonitor/service/SectionService.java
@@ -1,11 +1,5 @@
 package com.github.performancemonitor.service;
 
-import java.util.List;
-
-import com.github.performancemonitor.model.entity.Section;
-
 public interface SectionService {
-
-    List<Section> getDepartmentSections(long departmentId);
 
 }
