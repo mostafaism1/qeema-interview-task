c6a94ba8b67777480a48bfbf22041fd501f2c498
mostafa <me.cssign@gmail.com>
2021-06-30 08:59:06 +0200

    Add an endpoint to get a section's percent (i.e. its performance)

---
+++ backend/src/main/java/com/github/performancemonitor/controller/SectionController.java
@@ -0,0 +1,29 @@
+package com.github.performancemonitor.controller;
+
+import com.github.performancemonitor.model.dto.SectionPercentDto;
+import com.github.performancemonitor.model.entity.SectionPercent;
+import com.github.performancemonitor.model.mapper.SectionPercentToSectionPercentDtoMapper;
+import com.github.performancemonitor.service.SectionService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(path = "/sections")
+@AllArgsConstructor
+public class SectionController {
+
+    private final SectionService sectionService;
+    private final SectionPercentToSectionPercentDtoMapper sectionPercentMapper;
+
+    @GetMapping(path = "/{sectionId}/percent")
+    public SectionPercentDto getSectionPercent(@PathVariable(name = "sectionId") long sectionId) {
+        SectionPercent sectionPercent = sectionService.getSecionPercent(sectionId);
+        return sectionPercentMapper.apply(sectionPercent);
+    }
+
+}
