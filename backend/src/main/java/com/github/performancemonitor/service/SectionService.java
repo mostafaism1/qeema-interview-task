ea3c47bb26fbe531fe9ec4c024f7632d0c23bab4
mostafa <me.cssign@gmail.com>
2021-06-30 08:54:24 +0200

    Add service SectionService.getSectionPercent()

--- backend/src/main/java/com/github/performancemonitor/service/SectionService.java
+++ backend/src/main/java/com/github/performancemonitor/service/SectionService.java
@@ -1,5 +1,9 @@
 package com.github.performancemonitor.service;
 
+import com.github.performancemonitor.model.entity.SectionPercent;
+
 public interface SectionService {
+
+    SectionPercent getSecionPercent(long sectionId);
 
 }
