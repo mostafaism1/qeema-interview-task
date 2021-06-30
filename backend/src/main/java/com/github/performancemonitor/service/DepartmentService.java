c6c9acf1963e2efbbd0a710a80f734a04300c13b
mostafa <me.cssign@gmail.com>
2021-06-30 08:47:36 +0200

    Add service DepartmentService.getDepartmentMilestones()

--- backend/src/main/java/com/github/performancemonitor/service/DepartmentService.java
+++ backend/src/main/java/com/github/performancemonitor/service/DepartmentService.java
@@ -3,9 +3,12 @@ package com.github.performancemonitor.service;
 import java.util.List;
 
 import com.github.performancemonitor.model.entity.Department;
+import com.github.performancemonitor.model.entity.DepartmentMilestones;
 
 public interface DepartmentService {
 
     List<Department> getAllDepartments();
+
+    DepartmentMilestones getDepartmentMilestones(long departmentId);
 
 }
