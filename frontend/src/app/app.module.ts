722c638c676faa48809bfd2eb6848e937fd8d2b2
mostafa <me.cssign@gmail.com>
2021-06-30 17:08:25 +0200

    Add providers for Default services in AppModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { DefaultDepartmentService } from './shared/services/default-department.service';
+import { DefaultSectionService } from './shared/services/default-section.service';
+import { DepartmentService } from './shared/services/department.service';
+import { SectionService } from './shared/services/section.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: DepartmentService, useClass: DefaultDepartmentService },
+    { provide: SectionService, useClass: DefaultSectionService },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
