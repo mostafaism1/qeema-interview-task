29ff095c6456d776fde1fee8e8b78da8cbb1c486
mostafa <me.cssign@gmail.com>
2021-06-30 03:51:52 +0200

    Define a Department service

---
+++ backend/src/main/java/com/github/performancemonitor/service/DepartmentService.java
@@ -0,0 +1,11 @@
+package com.github.performancemonitor.service;
+
+import java.util.List;
+
+import com.github.performancemonitor.model.entity.Department;
+
+public interface DepartmentService {
+
+    List<Department> getAllDepartments();
+
+}
